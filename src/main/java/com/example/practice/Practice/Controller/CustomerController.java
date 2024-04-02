6dedc5a45221176d59fb6ee6927a7cbfbdfebd7f
shyammahadevan <shyam.mh07@gmail.com>
2024-04-02 18:36:22 +0530

    get call

--- src/main/java/com/example/practice/Practice/Controller/CustomerController.java
+++ src/main/java/com/example/practice/Practice/Controller/CustomerController.java
@@ -3,9 +3,12 @@ package com.example.practice.Practice.Controller;
 import com.example.practice.Practice.Controller.DAO.CustomerRepository;
 import com.example.practice.Practice.Entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -17,6 +20,11 @@ public class CustomerController {
     @PostMapping("/createCustomer")
     public Customer createRecord(@RequestBody Customer customer){
         return customerRepository.save(customer);
+    }
+
+    @GetMapping("getCustomers")
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 
 
